d68062de4c003f565068b454f0cf72efccdcf40f
stanley2058 <stanley2058@yahoo.com.tw>
2019-07-02 16:12:46 +0800

    fix script due to new changes made by appledaily

--- Appledaily UnBlocker.user.js
+++ Appledaily UnBlocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Appledaily UnBlocker
 // @namespace    STW
-// @version      1.0.1
+// @version      1.0.2
 // @description  unblock appledaily login
 // @author       STW
 // @match        http://*.appledaily.com/*
@@ -13,18 +13,41 @@
 // ==/UserScript==
 
 $.get(window.location.href, function(data) {
-    $('.ndArticle_leftColumn').html($(data).find('.ndArticle_leftColumn').html().replace('userLogin = OMOSDK.auth().getUserInfo().isLoggedIn || false;', 'userLogin = true;'));
-    unblock();
-});
+    let html = $(data).find('.ndArticle_leftColumn').html();
+    let article = $(data).find('.ndArticle_margin').html();
 
-function unblock() {
-    $('.ndPaywall').remove();
-    $('.ndArticle_margin').show();
-    $('.mediabox').show();
+    if (html == undefined || article == undefined) return;
+    let token = article.split('document.createElement("')[1].split('");')[0];
+    article = article.replace(`<${token}>`, '').replace(`</${token}/>`, '');
+    html = html
+        .replace('userLogin = OMOSDK.auth().getUserInfo().isLoggedIn || false;', 'userLogin = true;')
+        .replace(' || omoUserType != 2', '')
+        .replace('visibility: hidden;', '')
+        .replace('document.createElement', 'document.createElement("placeholder"); //')
+        .replace('<div class="ndAritcle_headPic" style="visibility: hidden;">', '<div class="cp_ndAritcle_headPic" style="overflow: hidden; text-align: center; background-color: #f7f7f7;">');
 
+    $('.ndArticle_leftColumn').html(html);
+    $('.ndArticle_margin').hide();
+
+    let newDiv = document.createElement('div');
+    newDiv.style.marginTop = "25px";
+    let newP = document.createElement('p');
+    newP.style.fontSize = "20px";
+    newP.style.lineHeight = "35px";
+    newP.style.marginBottom = "25px";
+    newP.innerHTML = article;
+    newDiv.appendChild(newP);
+    document.querySelector('.ndArticle_content').appendChild(newDiv);
+
+    let img = document.querySelector('.cp_ndAritcle_headPic img');
+    if (img) {
+        img.style.display = "inline-block";
+        img.style.maxWidth = "100%";
+    }
     let videoTag = $('#videobox script:not(script[type="text/javascript"])');
     if (videoTag.length > 0 && $('#video_player').length === 0) {
         let videoURL = videoTag[0].innerText.split('videoUrl = ')[1].split(';')[0].replace(/[']/g, '').replace(/["]/g, '');
+        console.log(videoURL);
 
         let player = document.createElement('video');
         let vidSrc = document.createElement('source');
@@ -40,4 +63,4 @@ function unblock() {
         document.getElementById('videobox').appendChild(player);
         document.getElementById('videobox').style.paddingLeft = '30px';
     }
-}
+});
